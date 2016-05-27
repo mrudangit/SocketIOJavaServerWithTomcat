0d31d6b43c3d9cd8b57a5886ac535adf7e847a6a
MRUDANG MAJMUDAR <mrudang.majmudar@jpmorgan.com>
2016-05-26 21:08:25 -0400

    sync

---
+++ src/main/java/com/solutionarchitects/SocketIOConnectionHandler.java
@@ -0,0 +1,71 @@
+package com.solutionarchitects;
+
+import com.google.gson.Gson;
+import com.google.gson.internal.StringMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by e211303 on 5/25/2016.
+ */
+public class SocketIOConnectionHandler {
+
+    private final WebSocketSession webSocketSession;
+    private final String nameSpace;
+    private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    private final String sessionid;
+
+    public SocketIOConnectionHandler(WebSocketSession webSocketSession , String sessionId, String nameSpace){
+        this.webSocketSession = webSocketSession;
+        this.sessionid= sessionId;
+        this.nameSpace = nameSpace;
+    }
+
+
+
+    public void eventReceived(String eventName, StringMap payload){
+
+        logger.info("Event Received Namespace : {}  EventName {} Payload {}", nameSpace,eventName,payload);
+
+        HashMap data = new HashMap();
+
+
+        data.put("Name","Mrudang");
+
+        sendEvent("news", data);
+
+    }
+
+
+    private void sendEvent(String eventName, Object payload){
+
+        ArrayList eventPayload = new ArrayList();
+
+        eventPayload.add(eventName);
+        eventPayload.add(payload);
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(eventPayload);
+
+        String eventMessage = String.format("%d%d%s,%s", 4, 2, nameSpace, json);
+
+        try {
+            webSocketSession.sendMessage(new TextMessage(eventMessage));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+}
--- src/main/java/com/solutionarchitects/WebSocketHandler.java
+++ src/main/java/com/solutionarchitects/WebSocketHandler.java
@@ -10,148 +10,306 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class WebSocketHandler implements org.springframework.web.socket.WebSocketHandler {
 
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
-    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass().getName());
-
-
+    ConcurrentHashMap<WebSocketSession, String> webSocketSessionMap = new ConcurrentHashMap<>();
+    ConcurrentHashMap<String, SocketIOConnectionHandler> socketIOConnectionHandlerConcurrentHashMap = new ConcurrentHashMap<>();
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception {
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 
+        logger.info("New WebSocket Connection Established : Session ID = {}", session);
 
-        LOGGER.info("WebSocket Connection Initialized Session :{} ",webSocketSession);
 
         Gson gson = new Gson();
 
+        String sessionId = UUID.randomUUID().toString();
         Handshake h = new Handshake();
-        h.sid= UUID.randomUUID().toString();
-        h.upgrades = new String[]{};
+        h.sid= sessionId;
+        h.upgrades=new String[]{};
         h.pingInterval=25000;
         h.pingTimeout=60000;
 
+        TextMessage msg = new TextMessage(String.format("%d%s",0,gson.toJson(h)));
 
-        TextMessage t = new TextMessage(String.format("%d%s",0,gson.toJson(h)));
+        session.sendMessage(msg);
 
-        webSocketSession.sendMessage(t);
-        t = new TextMessage(String.format("%d%d",4,0));
+        msg =new TextMessage(String.format("%d%d",4,0));
 
-        webSocketSession.sendMessage(t);
+        session.sendMessage(msg);
+
+        webSocketSessionMap.put(session,sessionId);
+
+
 
     }
 
     @Override
-    public void handleMessage(WebSocketSession webSocketSession, WebSocketMessage<?> webSocketMessage) throws Exception {
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
 
-        LOGGER.info("Message Received on Session : {} Message : {}",webSocketSession,webSocketMessage);
+        logger.info("Message = {} Session ID : {}", message, session);
 
-        String payload = (String)webSocketMessage.getPayload();
+        String payload = (String)message.getPayload();
 
         try{
 
-            HandlePacket(webSocketSession,payload);
-
-        }catch(Exception exp){
-            LOGGER.error("Error handling message",exp);
+            HandlePacket(session,payload);
+        }catch (Exception exp){
+            logger.error("Error handlign packet", exp);
         }
 
     }
 
-    @Override
-    public void handleTransportError(WebSocketSession webSocketSession, Throwable throwable) throws Exception {
 
-        LOGGER.info("Transport Error : Reason :{} ", throwable.getMessage());
+    private void HandlePacket(WebSocketSession session , String packet) throws IOException {
+
+        PacketType packetType= ParsePacketType(packet);
+
+        switch (packetType){
+
+            case OPEN:
+                break;
+            case CLOSE:
+                break;
+            case PING:
+                HandlePingPong(session);
+                break;
+            case PONG:
+                break;
+            case MESSAGE:
+                HandleMessage(session,packet);
+                break;
+            case UPGRADE:
+                break;
+            case NOOP:
+                break;
+            case CONNECT:
+                break;
+            case DISCONNECT:
+                break;
+            case EVENT:
+                break;
+            case ACK:
+                break;
+            case ERROR:
+                break;
+            case BINARY_EVENT:
+                break;
+        }
+    }
+
+    private void HandlePingPong(WebSocketSession session) throws IOException {
+
+        logger.info("Received Ping ..... Sending Pong");
+
+        TextMessage t = new TextMessage(String.format("%d", 3));
+
+        session.sendMessage(t);
+    }
+
+    private void HandleEventMessage(WebSocketSession session ,String data){
+
+        try {
+            DecodeEventMessage(session,data);
+        }catch (Exception exp){
+
+            logger.error("Error decoding data", exp);
+        }
+
+    }
+
+
+    private void HandleConnect(WebSocketSession session, String data) throws IOException {
+
+
+        logger.info("Received Connected {}", data);
+        String nameSpace = DecodeConnectMessage(data);
+
+        TextMessage msg = new TextMessage(String.format("%d%d%s", 4, 0, nameSpace));
+
+        session.sendMessage(msg);
+
+
+        logger.info("Connected Namespace : {}",nameSpace);
+
+        String sessionId = webSocketSessionMap.get(session);
+        String sessionIdNamespaceKey= String.format("%s#%s",sessionId,nameSpace);
+
+
+        SocketIOConnectionHandler socketIOConnectionHandler = new SocketIOConnectionHandler(session,sessionId,nameSpace);
+
+        socketIOConnectionHandlerConcurrentHashMap.put(sessionIdNamespaceKey,socketIOConnectionHandler);
+
+
+
+
+
+    }
+
+    private void HandleOpen(WebSocketSession session, String data) throws IOException {
+
+        String nameSpace = data.substring(2);
+
+        TextMessage msg = new TextMessage(String.format("%d%d%s", 4, 0, nameSpace));
+        session.sendMessage(msg);
+
+
+    }
+
+
+    private void HandleMessage(WebSocketSession session, String data) throws IOException {
+
+        PacketType messageType = PacketType.valueOfInner(GetOpCode(data.charAt(1)));
+
+        switch (messageType){
+
+
+            case OPEN:
+                HandleOpen( session, data);
+            case NOOP:
+                break;
+            case CONNECT:
+                HandleConnect( session, data);
+                break;
+            case DISCONNECT:
+                break;
+            case EVENT:
+                HandleEventMessage(session,data);
+                break;
+            case ACK:
+                break;
+            case ERROR:
+                break;
+            case BINARY_EVENT:
+                break;
+        }
+    }
+
+    private int GetOpCode(char c){
+        return c-48;
+    }
+
+    private PacketType ParsePacketType(String data){
+
+
+        int c = GetOpCode(data.charAt(0));
+
+        return PacketType.valueOf(c);
+
+    }
+
+
+
+    private String DecodeConnectMessage(String message){
+
+        int index = 0;
+
+        PacketType packetType= PacketType.valueOf(GetOpCode(message.charAt(index)));
+        ++index;
+
+        PacketType messageType = PacketType.valueOfInner(GetOpCode(message.charAt(index)));
+        index++;
+
+        String nameSpace = null;
+
+        if('/' == message.charAt(index)) {
+
+
+            nameSpace=message.substring(index);
+
+        }
+
+
+
+
+        return nameSpace;
+
+    }
+
+
+    private void DecodeEventMessage(WebSocketSession webSocketSession, String message){
+
+        int index = 0;
+
+        PacketType packetType= PacketType.valueOf(GetOpCode(message.charAt(index)));
+        ++index;
+
+        PacketType messageType = PacketType.valueOfInner(GetOpCode(message.charAt(index)));
+        index++;
+
+        StringBuilder sb = new StringBuilder();
+
+        if('/' == message.charAt(index)) {
+
+            while(message.charAt(index) != ','){
+                sb.append(message.charAt(index++));
+            }
+
+            index++;
+
+        }
+
+        StringBuilder idSb = new StringBuilder();
+
+        while(Character.isDigit(message.charAt(index))){
+            idSb.append(message.charAt(index));
+            index++;
+        }
+
+        String ackId = idSb.toString();
+
+        String nameSpace = sb.toString();
+
+        String jsonData = message.substring(index);
+
+        logger.info("PacketType : [{}] MessageType : [{}] Namespace : [{}] AckId : [{}] JSON : {}", packetType,messageType,nameSpace,ackId,jsonData);
+
+        Gson gson = new Gson();
+
+        ArrayList arrayList = gson.fromJson(jsonData,ArrayList.class);
+
+        String eventName = (String) arrayList.get(0);
+        StringMap eventData = (StringMap) arrayList.get(1);
+
+        logger.info("Event Name = [{}]  Data : {} ",eventName,eventData);
+
+
+
+        String sessionId = webSocketSessionMap.get(webSocketSession);
+
+        String sessionNamespaceKey = String.format("%s#%s",sessionId,nameSpace);
+
+        SocketIOConnectionHandler socketIOConnectionHandler = socketIOConnectionHandlerConcurrentHashMap.get(sessionNamespaceKey);
+
+        socketIOConnectionHandler.eventReceived(eventName,eventData);
+
 
     }
 
     @Override
-    public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus closeStatus) throws Exception {
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
 
+        logger.warn("Transport Session ID  : {}  Error : {}",session.getId(),exception.getMessage());
+    }
 
-        LOGGER.info("WebSocket Connection closed session : {} Close Status : {}",webSocketSession,closeStatus);
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
 
+        logger.info("Connection Closed =========================== {} Session ID : {} ",closeStatus, session.getId());
     }
 
     @Override
     public boolean supportsPartialMessages() {
         return false;
     }
-
-
-
-
-    private void HandlePacket(WebSocketSession webSocketSession, String packet)throws java.io.IOException {
-
-        PacketType packetType = ParsePacketType(packet);
-
-        switch (packetType){
-
-            case Open:
-                break;
-            case Close:
-                break;
-            case Ping:
-                HandlePing(webSocketSession);
-                break;
-            case Pong:
-                break;
-            case Message:
-                break;
-            case Upgrade:
-                break;
-            case NoOp:
-                break;
-            case Connect:
-                break;
-            case DisConnect:
-                break;
-            case Event:
-                break;
-            case Ack:
-                break;
-            case Error:
-                break;
-            case BinaryEvent:
-                break;
-        }
-
-
-
-
-    }
-
-
-
-    private void HandlePing(WebSocketSession webSocketSession)throws java.io.IOException{
-
-
-        LOGGER.info("Received Ping ... sending Pong");
-
-        TextMessage t = new TextMessage(String.format("%d",3));
-
-        webSocketSession.sendMessage(t);
-
-    }
-
-
-
-    private int GetOpCode(char c){
-        return c-48;
-    }
-
-
-    private PacketType ParsePacketType(String data){
-
-        int opCode = GetOpCode(data.charAt(0));
-
-        return PacketType.parsePacketType(opCode);
-
-    }
-
 
 
 }
