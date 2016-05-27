9ffb489c25cc8482ad5e1e0b014d2db20ecfb6e6
Mrudang Majmudar <mrudang@yahoo.com>
2016-05-27 12:21:10 -0400

    further refactoring

--- src/main/java/com/solutionarchitects/SocketIoJavaServerApplication.java
+++ src/main/java/com/solutionarchitects/SocketIoJavaServerApplication.java
@@ -1,5 +1,7 @@
 package com.solutionarchitects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,8 +13,13 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 @EnableWebSocket
 public class SocketIoJavaServerApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(SocketIoJavaServerApplication.class.getName());
+
+
 	public static void main(String[] args) {
 
 		ConfigurableApplicationContext run = SpringApplication.run(SocketIoJavaServerApplication.class, args);
+
+		logger.info("Application Context Created : {}",run);
 	}
 }
